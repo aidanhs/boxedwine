535c0a6748a0e789908740f6f5ffbd6179c9a9a7
danoon2 <danoon2>
2012-07-18 17:49:28 +0000

    Forgot a file in the previous checkin

---
+++ project/visual studio 10/boxedwine/doshal/startup.c
@@ -0,0 +1,29 @@
+#include <windows.h>
+
+char args[10][256];
+
+void getArgs(int* argc, char* argv[10]) {
+	char* cmdline = GetCommandLineA();
+	int in_quotes = 0;
+	int count = 0;
+	int len = 0;
+	args[count][0]=0;
+	argv[0] = args[count];
+
+	while (*cmdline)
+    {
+        if ((*cmdline == '\t' || *cmdline == ' ') && !in_quotes) {						
+			count++;
+			argv[count] = args[count];
+			len = 0;
+		}
+        else if (*cmdline == '\"')
+        {
+            in_quotes = !in_quotes;
+        } else {
+			args[count][len++] = *cmdline;
+		}
+        cmdline++;
+    }
+	*argc = count;
+}
