24d34eec00702d4daa65c1e0765dae3f0b9d8322
kevodwyer <kevodwyer>
2016-12-27 20:32:29 +0000

    changes to emscripten shells.  upload/download now not shown by default. dropbox integration commented out. All these changes streamline the official build script. Have updated shell.html with recent changes. Documentation in buildFlags.txt brought up-to-date.

--- c/project/linux/createOfficialEmscriptenBuild.sh
+++ c/project/linux/createOfficialEmscriptenBuild.sh
@@ -5,10 +5,7 @@ rm build/*
 cp boxedwine.html build/
 cp boxedwine.js build/
 cp browserfs.min.js build/
-cp dropbox.min.js build/
-cp zee-worker.js build/
 cp boxedwine.html.mem build/
-cp jszip.min.js build/
 cp buildfiles/* build/
 cd build
 DATE=`date +%Y-%m-%d`
