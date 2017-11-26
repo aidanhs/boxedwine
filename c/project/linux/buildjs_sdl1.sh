2b24dbb07bfac0d351064a12f8b14f00e6a6a08f
kevodwyer <kevodwyer>
2017-11-26 14:52:10 +0000

    retire some old emscripten build scripts.

--- c/project/linux/buildjs_sdl1.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-emcc -Wall -Wno-unused-function -I../../include ../../source/sdl/*.c ../../platform/linux/*.c ../../source/emulation/*.c ../../source/io/*.c ../../source/kernel/*.c ../../source/kernel/devs/*.c ../../source/kernel/proc/*.c ../../source/kernel/loader/*.c ../../source/util/*.c ../../source/util/pbl/*.c ../../source/opengl/sdl/*.c ../../source/opengl/*.c -o boxedwine.html -O3 -DUNALIGNED_MEMORY=1 -DUSE_MMU -s USE_SDL=1 --preload-file root@/root -s TOTAL_MEMORY=603979776 --shell-file shell.html
-./packager.py
-mv package.js boxedwine.js
-gzip -9 boxedwine.data
-mv boxedwine.data.gz boxedwine.data
