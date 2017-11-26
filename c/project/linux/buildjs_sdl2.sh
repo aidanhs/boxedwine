e2bad2ab018a3ebe462f971b96c564e83cff0084
kevodwyer <kevodwyer>
2017-11-26 14:47:51 +0000

    updated emscripten build/shell files to work without zlib dependency.

--- c/project/linux/buildjs_sdl2.sh
+++ c/project/linux/buildjs_sdl2.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-emcc -Wall -Wno-unused-variable -Wno-parentheses-equality -Wno-unused-function -I../../include ../../source/sdl/*.c ../../platform/linux/*.c ../../source/emulation/*.c ../../source/io/*.c ../../source/kernel/*.c ../../source/kernel/devs/*.c ../../source/kernel/proc/*.c ../../source/kernel/loader/*.c ../../source/util/*.c ../../source/util/pbl/*.c ../../source/opengl/sdl/*.c ../../source/opengl/*.c -o boxedwine.html -O3 -DUNALIGNED_MEMORY=1 -DUSE_MMU -s USE_SDL=2 -DSDL2 --preload-file root@/root -s TOTAL_MEMORY=419430400 --shell-file shell.html
+emcc -Wall -Wno-unused-variable -Wno-parentheses-equality -Wno-unused-function -I../../include ../../source/sdl/*.c ../../platform/linux/*.c ../../source/emulation/*.c ../../source/emulation/cpu/*.c ../../source/emulation/softmmu/*.c ../../source/io/*.c ../../source/kernel/*.c ../../source/kernel/devs/*.c ../../source/kernel/proc/*.c ../../source/kernel/loader/*.c ../../source/util/*.c ../../source/util/pbl/*.c ../../source/opengl/sdl/*.c ../../source/opengl/*.c -o boxedwine.html -O3 -DUNALIGNED_MEMORY=1 -DUSE_MMU -s USE_SDL=2 -DSDL2 --preload-file root@/root -s TOTAL_MEMORY=536870912 --shell-file shell.html
 ./packager.py
 mv package.js boxedwine.js
 gzip -9 boxedwine.data
