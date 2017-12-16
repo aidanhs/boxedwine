367c0933c8a7e1347c04b91b392d584debefcb24
kevodwyer <kevodwyer>
2017-12-16 16:51:55 +0000

    update a few paths to match new beta filesystem. I still have a problem that wine wants to init .wine directory even though it is running fine on cmd line.

--- c/project/linux/buildjsfs_sdl2.sh
+++ c/project/linux/buildjsfs_sdl2.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 emcc -Wall -Wno-unused-variable -Wno-parentheses-equality -Wno-unused-function -I../../include ../../source/sdl/*.c ../../platform/linux/*.c ../../source/emulation/*.c ../../source/emulation/cpu/*.c ../../source/emulation/softmmu/*.c ../../source/io/*.c ../../source/kernel/*.c ../../source/kernel/devs/*.c ../../source/kernel/proc/*.c ../../source/kernel/loader/*.c ../../source/util/*.c ../../source/util/pbl/*.c ../../source/opengl/sdl/*.c ../../source/opengl/*.c -o boxedwine.html -O2 -DUNALIGNED_MEMORY=1 -DUSE_MMU -s WASM=1 -s USE_SDL=2 -DSDL2 -s TOTAL_MEMORY=536870912  --shell-file shellfs.html
 if [ "$1" = "remote" ] ; then
-    zip -r9qdgds 10m boxedwine.zip root -x root/usr/lib/i386-linux-gnu/wine/wine/**\*
+    zip -r9qdgds 10m boxedwine.zip root -x root/lib/wine/**\*
     if [ -d "dlls" ]; then
         STR="{\"dlls\": {"
         for file in `find dlls -maxdepth 1 -type f -name "*.*"`; do
